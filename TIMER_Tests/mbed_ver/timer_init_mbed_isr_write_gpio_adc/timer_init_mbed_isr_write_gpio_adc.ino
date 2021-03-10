66eb6d0d1b94dd8ce4db4c6c7999b420eccfec82
jntzoc <jntzoc@cougarnet.uh.edu>
2021-03-10 13:33:01 -0600

    Rewriting the data sequence for mux

--- TIMER_Tests/mbed_ver/timer_init_mbed_isr_write_gpio_adc/timer_init_mbed_isr_write_gpio_adc.ino
+++ TIMER_Tests/mbed_ver/timer_init_mbed_isr_write_gpio_adc/timer_init_mbed_isr_write_gpio_adc.ino
@@ -23,21 +23,17 @@
 using namespace mbed;
 
 /* Globals */ 
-DigitalOut bit_zero(PJ_11, 1);  // D2 on Portenta
-DigitalOut bit_one(PG_7, 1);    // D3
-DigitalOut bit_two(PC_7, 1);    // D4 
-DigitalOut bit_three(PC_6, 1);  // D5
-
-DigitalOut bit_four(PA_8, 1);   // D6
-DigitalOut bit_five(PI_0, 1);   // D7
-DigitalOut bit_six(PC_3, 1);    // D8
-DigitalOut bit_seven(PI_1, 1);  // D9
+// Using BusOut interface to combine 8 DigitalOut pins 
+// in order to write to all at the same time.
+// Order of pins reversed to bit order. (Endianness reversed)
+BusOut eeg_bus (PI_1, PC_3, PI_0, PA_8, PC_6, PC_7, PG_7, PJ_11); 
+int current_ch = 0; 
 
 #define NUM_OF_CH 64         // sampling 64 channels
 int mux_bits[NUM_OF_CH];        
 
 /* Function prototype(s) */
-void mbed_main(void ); 
+void mbed_new_main(void ); 
 
 /* Arduino code that runs once */ 
 void setup() {
@@ -50,31 +46,45 @@ void setup() {
 /* Arduino code that runs multiple times */ 
 void loop() {
 
-  mbed_main();
+  mbed_new_main();
 
 }
 
 /* User-defined functions */ 
 /* Timer ISR */ 
-void timerISR(void )
-{
-  // Write to one GPIO pin and switch to next one in sequence
-  
-}
+// void timerISR(void )
+// {
+//   // Write to one GPIO pin and switch to next one in sequence
+//   eeg_bus = mux_bits[current_ch]
+//   current_ch++;
+
+//   // Reset current_ch only when last channel is read
+//   if (current_ch == (NUM_OF_CH - 1))   // if current ch == 63
+//   {
+//     current_ch = 0; 
+//   }
+// }
 
 /* Main function for mbed platform */ 
-void mbed_main(void )
+void mbed_new_main(void )
 {
   // Fill in mux bits 
-  int sequence = 0x00; 
-  for (int i = 0; i < NUM_OF_CH; i++)
+  for (int sequence = 0; sequence < NUM_OF_CH; sequence++)
   {
-    mux_bits[i] = sequence; 
-    sequence++; 
+    mux_bits[sequence] = sequence; 
   }
 
-  /* Testing out whether the array was filled in correctly */ 
-  while (1) 
+  // // Initialize a hardware timer
+  // Ticker tim;                   
+
+  // float tim_frq = 600;          // 600 Hz
+  // float timeout_us = ( (1 / tim_frq) * 1e6 );   // period = 1.67ms
+  
+  // // Point hardware timer to a function once timeout (1,6666.6 us) is reached 
+  // tim.attach_us( &timerISR, timeout_us); 
+  
+  // Main loop for mbed platform 
+  while(1) 
   {
     for (int i = 0; i < NUM_OF_CH; i++)
     {
@@ -82,28 +92,12 @@ void mbed_main(void )
     }
   }
 
-  // Initialize a hardware timer
-  Ticker tim;                   
-
-  float tim_frq = 600;          // 600 Hz
-  float timeout_us = ( (1 / tim_frq) * 1e6 );   // period = 1.67ms
-  
-  // Point hardware timer to a function once timeout (1,6666.6 us) is reached 
-  tim.attach_us( &timerISR, timeout_us); 
-  
-  // Main loop for mbed platform 
-  while(1) 
-  {
-    
-  }
-
 }
 
 /*
  * Results:
  * - Timer interrupt rate @ 609.7 Hz
- * 
- * - Array verification pending... 
+ * - Array verification = done!
  */
 
 
